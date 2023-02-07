7666051553e683f9ef5a3652515b88fc78302aed
huhuan <2960036258@qq.com>
2023-02-07 11:24:41 +0800

    帖子评论功能实现

--- src/pages/Posts/components/PostsList.tsx
+++ src/pages/Posts/components/PostsList.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react'
 import ProCard from "@ant-design/pro-card";
-import {LikeOutlined, MessageOutlined} from '@ant-design/icons';
+import {DeleteOutlined, LikeOutlined, MessageOutlined, WarningOutlined} from '@ant-design/icons';
 import {
   Avatar,
   Button,
@@ -9,7 +9,7 @@ import {
   Image,
   Input,
   List,
-  message,
+  message, Popconfirm,
   Popover,
   Space,
   Spin,
@@ -22,9 +22,12 @@ import {PostTag} from "@/models/postTag";
 import {Link} from 'umi';
 import {PostsComment} from "@/models/postsComment";
 import {getPostsDetails} from "@/services/posts";
-import minimatch from "minimatch";
-import {replyPostsComment} from "@/services/postsComment";
+import {deletePostsComment, replyPostsComment} from "@/services/postsComment";
 
+const UpdateCommentContext = React.createContext({
+  update: () => {
+  }
+});
 const {Text} = Typography;
 const PostTags = ({tag}: { tag: PostTag }) => {
   return (
@@ -45,7 +48,8 @@ const CommentLink: React.FC<{ text: string, size?: number, openid: string }> = (
     </Link>
   )
 }
-const PostCommentReply: React.FC<{ reply: PostsComment[] }> = ({reply}) => {
+
+const Reply: React.FC<{ pid: number, triggerComponentName: string, cParentId?: number }> = (props) => {
   const [replyContent, setReplyContent] = useState<string>('');
   const [openReplyInput, setOpenReplyInput] = useState<boolean>(false);
   const handleOpenChange = (newOpen: boolean) => {
@@ -55,24 +59,62 @@ const PostCommentReply: React.FC<{ reply: PostsComment[] }> = ({reply}) => {
     setOpenReplyInput(false);
   }
   const sendReply = async () => {
-    const params: { pId: number; openid: string; cParentId: number; comment: string } = {
-      pId: reply[0].pId,
+    const params: { pId: number; openid: string; comment: string, cParentId?: number } = {
+      pId: props.pid,
       openid: 'olG-q5aFDk6wc4tR446WUp3Gct1U',//TODO:评论人的身份：假设该openid为官方openid
-      cParentId: reply[0].cParentId,
       comment: replyContent,
+      cParentId: props.cParentId
     }
     const res = await replyPostsComment(params);
     if (res.data) {
       console.log(res.data.data)
-      setTimeout(() => {
-        hideReplyInput();
-        message.success("评论成功")
-      }, 500)
+      hideReplyInput();
+      message.success("评论成功");
+      return true;
     } else {
       message.error('操作失败，请重试');
+      return false;
     }
-    console.log(reply);
   }
+  return (
+    <>
+      <UpdateCommentContext.Consumer>
+        {context => {
+          let actionComponent;
+          if (props.triggerComponentName === 'button') {
+            actionComponent = <Button type={"primary"} icon={<MessageOutlined/>}>评论</Button>
+          } else if (props.triggerComponentName === 'link') {
+            actionComponent = <Typography.Link onClick={() => {
+            }}>回复</Typography.Link>
+          } else if (props.triggerComponentName === 'icon') {
+            actionComponent = <MessageOutlined onClick={() => {
+            }}/>
+          }
+          return (
+            <Popover open={openReplyInput} onOpenChange={handleOpenChange} placement="bottomLeft" title={
+              <Space>
+                <Input size={"small"} placeholder={"输入回复"} value={replyContent} onChange={e => {
+                  setReplyContent(e.target.value);
+                }}/>
+                <Button type={"primary"} size={"small"}
+                        onClick={() => {
+                          sendReply().then(() => {
+                            context.update()
+                          }).catch((err: any) => {
+                            console.log(err)
+                          })
+                        }}>发送</Button>
+              </Space>
+            } trigger="click">
+              {actionComponent}
+            </Popover>
+          )
+        }}
+      </UpdateCommentContext.Consumer>
+    </>
+  )
+}
+const PostCommentReply: React.FC<{ reply: PostsComment[] }> = ({reply}) => {
   return (
     <>
       <Space align={"start"} direction={"vertical"}>
@@ -93,18 +135,7 @@ const PostCommentReply: React.FC<{ reply: PostsComment[] }> = ({reply}) => {
               </Typography.Text>
             </Space>
             <Typography.Text>{item.comment}</Typography.Text>
-            <Popover open={openReplyInput} onOpenChange={handleOpenChange} placement="bottomLeft" title={
-              <Space>
-                <Input size={"small"} placeholder={"输入回复"} value={replyContent} onChange={e => {
-                  setReplyContent(e.target.value);
-                }}/>
-                <Button type={"primary"} size={"small"} onClick={() => sendReply()}>发送</Button>
-              </Space>
-            } trigger="click">
-              <Typography.Link>
-                回复
-              </Typography.Link>
-            </Popover>
+            <Reply pid={item.pId} cParentId={item.cParentId} triggerComponentName={"link"}/>
           </Space>
           {item.postsCommentList.length === 0 ? <></> : <PostCommentReply reply={item.postsCommentList}/>}
         </div>)}
@@ -112,46 +143,17 @@ const PostCommentReply: React.FC<{ reply: PostsComment[] }> = ({reply}) => {
     </>
   )
 }
-let refreshComment = false;
-const Reply: React.FC<{ pid: number }> = (props) => {
-  const [replyContent, setReplyContent] = useState<string>('');
-  const [openReplyInput, setOpenReplyInput] = useState<boolean>(false);
-  const handleOpenChange = (newOpen: boolean) => {
-    setOpenReplyInput(newOpen);
-  };
-  const hideReplyInput = () => {
-    setOpenReplyInput(false);
-  }
-  const sendReply = async () => {
-    const params: { pId: number; openid: string; comment: string } = {
-      pId: props.pid,
-      openid: 'olG-q5aFDk6wc4tR446WUp3Gct1U',//TODO:评论人的身份：假设该openid为官方openid
-      comment: replyContent,
-    }
-    const res = await replyPostsComment(params);
-    if (res.data) {
-      console.log(res.data.data)
-      hideReplyInput();
-      message.success("评论成功");
-      refreshComment = !refreshComment;
-    } else {
-      message.error('操作失败，请重试');
-    }
-  }
-  return (
-    <Popover open={openReplyInput} onOpenChange={handleOpenChange} placement="bottomLeft" title={
-      <Space>
-        <Input size={"small"} placeholder={"输入回复"} value={replyContent} onChange={e => {
-          setReplyContent(e.target.value);
-        }}/>
-        <Button type={"primary"} size={"small"} onClick={() => sendReply()}>发送</Button>
-      </Space>
-    } trigger="click">
-      <Button type={"primary"} icon={<MessageOutlined/>}>评论</Button>
-    </Popover>
-  )
-}
 const PostCommentItem: React.FC<{ comment: PostsComment }> = ({comment}) => {
+  const confirmDeleteComment = async () => {
+    const res = await deletePostsComment({cId: comment.cId})
+    if (res.data) {
+      message.success("删除评论成功")
+      return true;
+    } else {
+      message.error("删除评论失败")
+      return false;
+    }
+  }
   return (
     <>
       <Space align={"start"}>
@@ -166,7 +168,28 @@ const PostCommentItem: React.FC<{ comment: PostsComment }> = ({comment}) => {
               <Typography.Text>{comment.commentDate}</Typography.Text>
             </div>
             <Space>
-              <MessageOutlined style={{cursor: 'pointer'}}/>
+              <Reply
+                cParentId={comment.cId}
+                pid={comment.pId}
+                triggerComponentName={"icon"}/>
+              <UpdateCommentContext.Consumer>
+                {context => {
+                  return (
+                    <Popconfirm title={"确认删除评论？"}
+                                onConfirm={() => {
+                                  confirmDeleteComment()
+                                    .then(() => {
+                                      context.update()
+                                    }).catch(() => {
+                                    message.error("删除评论失败！")
+                                  })
+                                }} placement="left"
+                                icon={<WarningOutlined style={{color: "red"}}/>}>
+                      <DeleteOutlined style={{cursor: "pointer"}}/>
+                    </Popconfirm>
+                  )
+                }}
+              </UpdateCommentContext.Consumer>
             </Space>
           </Space>
         </Space>
@@ -199,89 +222,101 @@ const PostComment: React.FC<{ refresh: boolean, pid: number }> = (props) => {
     </>
   )
 }
+
 const PostsList: React.FC<{ postList: any, spin: boolean }> = ({postList, spin}) => {
   const [showCommentDrawer, setShowCommentDrawer] = useState<boolean>(false);
   const [currentPid, setCurrentPid] = useState<number>(0);
+  const [refreshComment, setRefreshComment] = useState<boolean>(true);
+  const updateComment = React.useCallback(() => {
+    setRefreshComment(!refreshComment)
+  }, [refreshComment])
   return (
-    <ProCard>
-      <Spin spinning={spin}>
-        <List
-          itemLayout="vertical"
-          size="default"
-          pagination={{
-            onChange: (page) => {
-              console.log(page);
-            },
-            pageSize: 10,
-          }}
-          dataSource={postList}
-          renderItem={(item: PostsSimpleType) => (
-            <List.Item
-              key={item.pid}
-              actions={[
-                <>
-                  <Space split={<Divider type="vertical"/>}>
-                    <Space style={{cursor: "pointer"}} key={"like"}>
-                      {React.createElement(LikeOutlined)}
-                      {item.currentPostsLikeCount.toString()}
+    <>
+      <ProCard>
+        <Spin spinning={spin}>
+          <List
+            itemLayout="vertical"
+            size="default"
+            pagination={{
+              onChange: (page) => {
+                console.log(page);
+              },
+              pageSize: 10,
+            }}
+            dataSource={postList}
+            renderItem={(item: PostsSimpleType) => (
+              <List.Item
+                key={item.pid}
+                actions={[
+                  <>
+                    <Space split={<Divider type="vertical"/>}>
+                      <Space style={{cursor: "pointer"}} key={"like"}>
+                        {React.createElement(LikeOutlined)}
+                        {item.currentPostsLikeCount.toString()}
+                      </Space>
+                      <Space style={{cursor: "pointer"}} key={"comment"} onClick={() => {
+                        setCurrentPid(item.pid)
+                        setShowCommentDrawer(true)
+                        setRefreshComment(!refreshComment)
+                      }}>
+                        {React.createElement(MessageOutlined)}
+                        {item.currentPostsCommentCount.toString()}
+                      </Space>
                     </Space>
-                    <Space style={{cursor: "pointer"}} key={"comment"} onClick={() => {
-                      setCurrentPid(item.pid)
-                      setShowCommentDrawer(true)
-                    }}>
-                      {React.createElement(MessageOutlined)}
-                      {item.currentPostsCommentCount.toString()}
+                  </>
+                ]}
+              >
+                <Space direction="vertical" size={[10, 12]}>
+                  <List.Item.Meta
+                    title={
+                      <Typography.Link ellipsis={true}>
+                        {item.content}
+                      </Typography.Link>
+                    }
+                    description={<PostTags tag={item.tag}/>}
+                  />
+                  {item.content}
+                  {/*用户信息*/}
+                  <div>
+                    <Space>
+                      <Avatar
+                        size="small"
+                        src={<Image src={item.miniUser.avatar} alt={"用户头像"}/>}
+                      />
+                      <Link to={`/miniUser/center/${item.miniUser.openid}`}>{item.miniUser.username}</Link>
+                      <Text style={{fontSize: '14px'}} strong={true}>发布于</Text>
+                      <Text style={{fontSize: '12px'}} type={"secondary"}>{item.sendTime}</Text>
                     </Space>
-                  </Space>
-                </>
-              ]}
-            >
-              <Space direction="vertical" size={[10, 12]}>
-                <List.Item.Meta
-                  title={
-                    <Typography.Link ellipsis={true}>
-                      {item.content}
-                    </Typography.Link>
-                  }
-                  description={<PostTags tag={item.tag}/>}
-                />
-                {item.content}
-                {/*用户信息*/}
-                <div>
-                  <Space>
-                    <Avatar
-                      size="small"
-                      src={<Image src={item.miniUser.avatar} alt={"用户头像"}/>}
-                    />
-                    <Link to={`/miniUser/center/${item.miniUser.openid}`}>{item.miniUser.username}</Link>
-                    <Text style={{fontSize: '14px'}} strong={true}>发布于</Text>
-                    <Text style={{fontSize: '12px'}} type={"secondary"}>{item.sendTime}</Text>
-                  </Space>
-                </div>
-                {/*图片列表组件*/}
-                <div>
-                  <Space size={[12, 6]} wrap>
-                    {item.postsImgList.map((img: PostsImg, index) => <RightImg key={index} imgUrl={img.imgUrl}/>)}
-                  </Space>
-                </div>
-              </Space>
-            </List.Item>
-          )}
-        >
-          <Drawer onClose={() => {
-            setShowCommentDrawer(false)
-          }}
-                  open={showCommentDrawer}
-                  closable={false}
-                  title={"评论列表"}
-                  extra={<Reply pid={currentPid}/>}
-                  width={520}
+                  </div>
+                  {/*图片列表组件*/}
+                  <div>
+                    <Space size={[12, 6]} wrap>
+                      {item.postsImgList.map((img: PostsImg, index) => <RightImg key={index} imgUrl={img.imgUrl}/>)}
+                    </Space>
+                  </div>
+                </Space>
+              </List.Item>
+            )}
           >
-            <PostComment refresh={showCommentDrawer} pid={currentPid}/>
-          </Drawer>
-        </List>
-      </Spin>
-    </ProCard>
+          </List>
+          <UpdateCommentContext.Provider value={{update: updateComment}}>
+            <Drawer onClose={() => {
+              setShowCommentDrawer(false)
+            }}
+                    open={showCommentDrawer}
+                    closable={false}
+                    title={"评论列表"}
+                    extra={<Reply
+
+                      pid={currentPid} triggerComponentName={"button"}/>}
+                    width={520}
+            >
+              <PostComment refresh={refreshComment} pid={currentPid}/>
+            </Drawer>
+          </UpdateCommentContext.Provider>
+        </Spin>
+      </ProCard>
+    </>
   )
 }
 
--- src/services/postsComment.ts
+++ src/services/postsComment.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import {PostsComment} from "@/models/postsComment";
 
 /**
  * 评论帖子接口
@@ -15,5 +14,20 @@ export function replyPostsComment(params: { openid: string; pId: number; comment
     }).catch((err: any) => {
       console.error(`评论失败`, err);
       return {}
+    })
+}
+
+/**
+ * 删除帖子评论接口
+ * @param params
+ */
+export function deletePostsComment(params: { cId: number }) {
+  return axios.post("/server/postsComment/deletePostsComment?cId=" + params.cId)
+    .then((res: any) => {
+      console.log(`删除评论成功！`)
+      return res;
+    }).catch((err: any) => {
+      console.error(`删除评论失败！`, err);
+      return {};
     })
 }
